94f671117a18f9d80b6f11063f4d13f5ab3ca8cc
ctfgate <ctfgate@example.com>
2019-09-10 17:14:30 +0900

    Add web04

---
+++ challenges/web04/README.md
@@ -0,0 +1,14 @@
+# [Web 50] Dream Job
+
+## Description
+フラグはLinuxのパスワードファイルにあります。
+
+The flag is in the Linux password file.
+
+`http://<redacted>:10004`
+
+## Solution
+https://scrapbox.io/kayuminbou/Dream_Job
+
+## Flag
+`ctfgate{c4n_y0u_7r4v3r53?}`
---
+++ challenges/web04/web/pages/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Dream Job</title>
+</head>
+
+<body>
+    <?php
+        define('TMPLDIR', '/var/www/html/tmpl/');
+        $tmpl = filter_input(INPUT_GET, 'year');
+    ?>
+    <p>日本FP協会の発表している "小学生の「将来なりたい職業」ランキングトップ10" について紹介するサイトです。</p>
+    <form action="/" method="GET">
+    <p>年度選択<br>
+    <select name="year">
+    <option value="2018-man.html">2018年度 男子</option>
+    <option value="2018-woman.html">2018年度 女子</option>
+    <option value="2017-man.html">2017年度 男子</option>
+    <option value="2017-woman.html">2017年度 女子</option>
+    </select></p>
+    <input type="submit" value="決定">
+    </form>
+    <br>
+    <?php readfile(TMPLDIR . $tmpl); ?>
+</body>
+</html>
