6e5d0428ede43157aa0582c3e9d1ecda4b265150
ctfgate <ctfgate@example.com>
2019-09-13 18:16:31 +0900

    Change crawling time

--- challenges/web03/web/pages/a140872db281f3a2dda3f683cd2b76ef.php
+++ challenges/web03/web/pages/a140872db281f3a2dda3f683cd2b76ef.php
@@ -3,7 +3,7 @@ $json_file="a140872db281f3a2dda3f683cd2b76ef.json";
 $arr = array();
 $arr[] = [
     'time' => "9999/99/99 00:00:00",
-    'text' => "These messages are deleted every 10 minutes.",
+    'text' => "These messages are deleted every 20 minutes.",
 ];
 $en_json = json_encode($arr);
 file_put_contents($json_file, $en_json, LOCK_EX);
--- challenges/web12/web/pages/360cc615784d8b7fd34024420f38b6e0.php
+++ challenges/web12/web/pages/360cc615784d8b7fd34024420f38b6e0.php
@@ -1,9 +1,9 @@
 <?php
-$json_file="c0b0cc46d638c27d7b439331656755e2.json";
+$json_file="360cc615784d8b7fd34024420f38b6e0.json";
 $arr = array();
 $arr[] = [
     'time' => "9999/99/99 00:00:00",
-    'text' => "These messages are deleted every 10 minutes.",
+    'text' => "These messages are deleted every 20 minutes.",
 ];
 $en_json = json_encode($arr);
 file_put_contents($json_file, $en_json, LOCK_EX);
